51c0484ba05d6aac08457b4726b993734d3016cb
Gastón Alvarez <gaston_a96@hotmail.com>
2023-09-09 17:08:47 -0300

    small fixes

--- src/ui/components/AtomItem.tsx
+++ src/ui/components/AtomItem.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "../styles/components/AtomItem.css"
 
+
 export default function AtomItem({ atom }:any) {
+
   return (
     <div className="atom">{atom}</div>
   )
--- src/ui/components/Marquee.tsx
+++ src/ui/components/Marquee.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import "../styles/components/Marquee.css"
+import React from "react";
+import "../styles/components/Marquee.css";
 
-export default function Marquee({ children }:any) {
-  return (
-    <div className="marquee">
-      <div className="marquee-content">
+export default function Marquee({ children }: any) {
+	return (
+		<div className="marquee">
+			<div className="marquee-content">
         {
           children
         }
       </div>
-    </div>
-  )
+		</div>
+	);
 }
